199bc2d14d657b2748780bbdb8d49d2f58ee7d05
snir-sh <sshilderman@gmail.com>
2015-05-19 09:36:27 +0300

    GREEN: pass test for consistent

--- Crowdmap.py
+++ Crowdmap.py
@@ -2,5 +2,17 @@ class Crowdmap(object):
 	def __init__(self, init_list):
 		self.list = init_list
 
-    def get_all_posts_for(self, name):
-        return [post for post in self.list if post.find(name)!=-1]
+	def get_all_posts_for(self, name):
+		return [post for post in self.list if post.find(name) != -1]
+		
+	def is_location_for_name(self, name):
+		posts = [post for post in self.list if post.find(name) != -1 and post.find('at') != -1]
+		return len(posts) != 0
+		
+	def check_consistent(self, name):
+		appears = 0
+		for post in self.list:
+			if post.find(name):
+				appears = appears + 1
+		return appears <= 1 
+		
--- FindAPerson.py
+++ FindAPerson.py
@@ -1,13 +1,28 @@
 import unittest
 from Crowdmap import Crowdmap
 
-class FindAPersonTests(unittest.TestCase):
-    def setUp(self):
-        self.crowdmap = Crowdmap(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"])
+class FindAPerson(unittest.TestCase):
+	def setUp(self):
+		self.crowdmap = Crowdmap(["I met Or A. at Chabad house Bangkok", 
+									"We found Or A. R.I.P at Langtang valley",
+									"MissingCowboy",
+									"Lassy Come Home"])
 		
-    def test_getAllPostsForName(self):
-        posts = self.crowdmap.get_all_posts_for("Or")
-        self.assertIn(posts, ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"])
-						
+	def test_getAllPostsForName(self):
+		posts = self.crowdmap.get_all_posts_for("Or")
+		self.assertEquals(posts, ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"])
+
+	def test_getAllPostForMissingName(self):
+		posts = self.crowdmap.get_all_posts_for("Joe")
+		self.assertEquals([], posts)
+		
+	def test_existingLocationInformationReturnsTrue(self):
+		location_exist = self.crowdmap.is_location_for_name("Or")
+		self.assertTrue(location_exist)
+		
+	def test_checkMapConsistent(self):
+		appears = self.crowdmap.check_consistent("Or")
+		self.assertFalse(appears)
+		
 if __name__ == '__main__':
 	unittest.main()
